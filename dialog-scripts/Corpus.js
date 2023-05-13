f8ea2a419258ca068bc2b908fbf3bab9d0d9d6da
charlesoni <charlion78@gmail.com>
2023-05-13 19:59:10 +0200

    exchange ai commit

---
+++ dialog-scripts/Corpus.js
@@ -0,0 +1,52 @@
+// This example demonstrates how to add a text AI chat to your web page, website or web app.
+// In this example, you can find corpus for Q&A answering and commands to navigate in the web page.
+// Get the web page with the text AI chat for this example here: https://github.com/alan-ai/website-chat-demo.
+// Check out a live demo at: https://alan-ai.github.io/website-chat-demo/.
+
+// Web page corpus
+corpus(
+    {url: "https://alan-ai.github.io/website-chat-demo/", depth: 1},
+);
+
+// Text corpus
+corpus(`
+    Hi, I'm coinhaven AI assistant that can help you with information on trade, build investment, and risk managment for the Coinhaven Trading Platform
+    welcome to Coinhaven Trading Platform.
+    Founded in 2019 in the major European disruptive technology hotspot of Berlin.
+    Coinhaven is a cryptocurrency exchange and trading platform.
+    We offer a wide range of cryptocurrencies from BTC, ETH, XRP, LTC, BCH, EOS, XLM, DASH, ZEC, ETC, ZRX, REP, BAT, LINK, OMG, XTZ, ALGO, KNC, OXT, DAI, COMP, BAND, NMR, MANA, ENJ, CVC, KAVA, USDC, and USDT.
+    We are a fully regulated and licensed exchange, adhering to the strictest security standards.
+    Coinhaven is an all-in-one trading and exchange platform for desktop and, via the Coinhaven app
+    mobile devices where users can easily buy Bitcoin and other digital money with a variety of fiat currencies
+    including USD, EUR, GBP, and RUB.
+    Coinhaven is a cryptocurrency exchange and trading platform.
+    We offer a wide range of cryptocurrencies from BTC, ETH, XRP, LTC, BCH, EOS, XLM, DASH, ZEC, ETC, ZRX, REP, BAT, LINK, OMG, XTZ, ALGO, KNC, OXT, DAI, COMP, BAND, NMR, MANA, ENJ, CVC, KAVA, USDC, and USDT.
+    We are a fully regulated and licensed exchange, adhering to the strictest security standards.
+    Users can also exchange their Bitcoin for a wide range of crypto assets such as Ethereum with just a few clicks
+    Other services include an intuitive OTC brokerage interface, Coinhaven wallet (personal fiat and cryptocurrency wallets)
+    and Coinhaven Earn (a cryptocurrency interest-earning program)and derivatives trading.
+    Available in 110 countries around the world, Coinhavens mission is to create an open financial market accessible to everyone.
+    Crypto exchange easily buy, sell and swap crypto assets through a frictionless, intuitive and user-friendly interface. Coinhaven offers low withdrawal commissions, low trading fees and regular discounts.
+    Coinhaven wallet a secure and easy-to-use wallet to store, send and receive Bitcoin and other cryptocurrencies. Users can also send Bitcoin to an email address.  
+    Coinhaven Earn a cryptocurrency interest-earning program where users can earn up to 5% interest on their Bitcoin holdings.
+    OTC brokerage interface buy crypto over the counter minus the high brokerage fees. Ideal for institutions that seek a fast, reliable and secure transaction process.
+    Coinhaven Pro a professional trading platform for individual and institutional traders with advanced trading tools and charting.
+    Coinhaven API a powerful tool for institutional traders and other advanced users who need to execute trades and transactions at high frequency.
+    Coinhaven Markets a platform for institutional traders to buy and sell large amounts of Bitcoin and other cryptocurrencies.
+    Coinhaven Prime a platform for institutional traders to buy and sell large amounts of Bitcoin and other cryptocurrencies.
+    Coinhaven Custody a secure and insured custody service for institutions and high net worth individuals.
+    Coinhaven Research a comprehensive research platform that provides market insights and data to help traders make informed decisions.
+    Coinhaven Learn a free educational platform that provides guides and tutorials on how to trade and invest in Bitcoin and other cryptocurrencies.
+    Coinhaven Blog a blog that provides the latest news and updates on the cryptocurrency market.
+    Coinhaven Podcast a podcast that provides the latest news and updates on the cryptocurrency market.
+    More ground-breaking features and services are being added periodically, including Coinhaven's very own exchange token coming soon.
+    Coinhaven, your safe space in the crypto world.
+
+
+
+
+
+
+
+    
+`)
